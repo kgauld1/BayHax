9406c79dd07fabcb37ff0dc3dec22c95b77790cb
Michelle Bryson <mbryson562@gmail.com>
2020-06-06 19:26:19 +0000

    updated logo

--- public/scripts/viewer.js
+++ public/scripts/viewer.js
@@ -12,7 +12,7 @@ function changeHash(){
 		elements[i].style = "";
 	}
 
-	document.getElementById(location.hash.substring(1)).style = "border-left: 8px solid rgb(156, 116, 250);	background: rgb(240, 240, 240);"
+	document.getElementById(location.hash.substring(1)).style = "border-left: 8px solid rgb(156, 116, 250);	background: rgb(245, 245, 245);"
 }
 
 if (!location.hash) location.hash = "#dashboard";
