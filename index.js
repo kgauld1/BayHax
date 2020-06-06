1990a327ce3a38140e3d6b0642653aafb2d88fa6
Kevin Gauld <kgauld1@gmail.com>
2020-06-06 03:32:51 +0000

    initial commit

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.render('index');
+});
+
+app.post('/update', (req, res) => {
+
 });
 
 app.listen(3000, () => {
